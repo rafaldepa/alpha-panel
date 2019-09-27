05d6c962fdf853545b90e7bf4c59155b7bfaedcb
sirmovie <kontakt@rafaldepa.pl>
2019-09-27 16:32:10 +0200

    Login page

---
+++ README.md
@@ -0,0 +1,20 @@
+# Kancelaria Alpha - Panel
+
+## Usage
+
+### Installing
+Clone this project and use [npm](https://www.npmjs.com/) to install the required node-modules:
+```bash
+npm install
+```
+### Running development server
+To start the live preview of the development version use:
+```bash
+gulp serve
+```
+### Build a production version
+Build the production version with a single command:
+```bash
+gulp build
+```
+When the build finishes, your files will be available in the **dist** folder.
---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+const gulp = require('gulp');
+const del = require('del');
+const es = require('event-stream');
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+
+// Tasks
+gulp.task('sass', () => {
+    return gulp.src('app/scss/*.scss')
+        .pipe(sass())
+        .pipe(prefix('last 2 versions'))        
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('styles', () => {
+    return gulp.src('app/css/*.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('scripts', () => {
+    return gulp.src('app/js/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(minify({
+            ext: {
+                src: '.js',
+                min: '.js'
+            },
+            mangle: false,
+            noSource: true      
+        }))
+        .pipe(gulp.dest('dist/js'))        
+});
+
+gulp.task('clean', (cb) => {
+    del.sync('dist/*');
+    return cb();
+});
+
+gulp.task('move-files', (cb) => {
+    es.merge([
+        gulp.src('app/*.html').pipe(gulp.dest('dist/')),
+        gulp.src('app/images/*').pipe(gulp.dest('dist/images'))
+    ]);    
+    
+    return cb();
+});
+
+// Live Preview
+gulp.task('serve', () => {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    });
+    gulp.watch("app/scss/*.scss", gulp.series('sass'));
+    gulp.watch("app/js/*.js").on('change', browserSync.reload);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+// Build
+gulp.task('build', gulp.series('clean','styles','scripts','move-files'));
+
+// Default
+gulp.task('default', gulp.series('serve'));
